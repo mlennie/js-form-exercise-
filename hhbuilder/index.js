388974506acfbc3bc48c5772025910f6229f5fea
Monty Lennie <montylennie@gmail.com>
2017-03-09 11:41:15 +0000

    start serializing form

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+var familyMembers = [];
+
+function getDataForControl(control) {
+  var data = {};
+  var inputs = [].slice.call(e.target.getElementsByTagName(control));
+  inputs.forEach(input => {
+    data[input.name] = input.value;
+  });
+}
+
+function serialize(e) {
+  var data = {};
+  data = getDataForControl('input',data,e);
+  data = getDataForControl('select',data,e);
+}
+
+function newMemberSubmit(form,event) {
+debugger;
+
+}
