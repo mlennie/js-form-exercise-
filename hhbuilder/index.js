40a0795b0f3c955eb1d2b179d8c404efa6ad0352
Monty Lennie <montylennie@gmail.com>
2017-03-09 11:55:39 +0000

    finish serializing

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,20 +1,24 @@
 var familyMembers = [];
 
-function getDataForControl(control) {
-  var data = {};
+function getDataForControl(control,data,e) {
   var inputs = [].slice.call(e.target.getElementsByTagName(control));
-  inputs.forEach(input => {
-    data[input.name] = input.value;
-  });
+  for (var i = 0; i < inputs.length; i++) {
+    data[inputs[i].name] = inputs[i].value;
+  }
+  return data;
 }
 
 function serialize(e) {
   var data = {};
   data = getDataForControl('input',data,e);
   data = getDataForControl('select',data,e);
+  return data;
 }
 
-function newMemberSubmit(form,event) {
+function newMemberSubmit(form,e) {
+  var data = serialize(e);
+
 debugger;
+
 
 }
