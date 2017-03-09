d7ca96416a2bb030337df8293c0025ef66d2dcd8
Monty Lennie <montylennie@gmail.com>
2017-03-09 13:36:47 +0000

    fix propogation issues

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -12,28 +12,54 @@ function serialize() {
 }
 
 function dataValid(data) {
-  return data.age && +data.age > 0 && data.rel && data.rel.length > 0;
+  return data && data.age && +data.age > 0 && data.rel && data.rel.length > 0;
 }
 
-function newMemberSubmit() {
-debugger;
+function refreshList() {
+  var html, member,age,rel,smoker;
+  var list = document.getElementsByClassName('household')[0];
+  list.innerHTML = "";
+  for (var i = 0; i < familyMembers.length; i++) {
+    html = "";
+    member = familyMembers[i];
+    age = "<p>Age: " + member.age + "</p>";
+    rel = "<p>Relationship: " + member.rel + "</p>";
+    smoker = "<p>Smoker: " + member.smoker + "</p>";
+    html += "<li>"+age+rel+smoker+"</li><hr>";
+    list.insertAdjacentHTML('beforeend', html);
+  }
+}
+
+function newMemberSubmit(e) {
   var data = serialize();
   if (dataValid(data)) {
     familyMembers.push(data);
+    refreshList();
   } else {
     var message = "Data is not valid. Age is required and greater than 0." +
                   " Relationship is also required";
     alert(message);
   }
 
-debugger;
+  e.preventDefault()
+  e.stopPropagation();
+}
 
-
+function handleSubmit() {
+    e.preventDefault()
+    e.stopPropagation();
 }
 
 function addClickListener() {
-  document.getElementsByClassName("add")[0].addEventListener("click", newMemberSubmit);
+  document.getElementsByClassName("add")[0]
+          .addEventListener("click", newMemberSubmit);
 }
 
+function submitClickListener() {
+  document.forms[0]
+          .addEventListener("submit", handleSubmit);
+}
+
+submitClickListener();
 addClickListener();
 
