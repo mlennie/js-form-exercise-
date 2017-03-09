cf3ce4bdc6388832ed74c2fedb2e48930bba12ba
Monty Lennie <montylennie@gmail.com>
2017-03-09 12:27:59 +0000

    add validation and add member to list

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -11,8 +11,23 @@ function serialize(e) {
   return data;
 }
 
+function dataValid(data) {
+  return data.age && +data.age > 0 && data.rel && data.rel.length > 0;
+}
+
+function addMemberToList(data) {
+  familyMembers.push(data);
+}
+
 function newMemberSubmit(form,e) {
   var data = serialize(e);
+  if (dataValid(data)) {
+    addMemberToList(data);
+  } else {
+    var message = "Data is not valid. Age is required and greater than 0." +
+                  " Relationship is also required";
+    alert(message);
+  }
 
 debugger;
 
