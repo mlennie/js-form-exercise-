f908c064faf901fa8b417a3e7e9a17e73ba2ddf7
Monty Lennie <montylennie@gmail.com>
2017-03-10 14:24:01 +0000

    use yaml

--- slcsp/slcsp_calculator.rb
+++ slcsp/slcsp_calculator.rb
@@ -1,10 +1,11 @@
 require 'csv'
+require 'yaml'
 
 class SlcspCalculator
 
   def self.parse_slcsp
     CSV.foreach("./slcsp.csv") do |row|
-      puts row.inspect
+      puts YAML.load(row.inspect)[0]
     end
   end
 end
