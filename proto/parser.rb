9f84665421c51efd60f3fef45f9643dd7be4ddab
Monty Lennie <montylennie@gmail.com>
2017-03-24 01:17:49 +0000

    get uids matching

--- proto/parser.rb
+++ proto/parser.rb
@@ -14,15 +14,15 @@ autopays_ended = 0
 user_balance = 0.0000000000
 
 # parse header
-magic_string = io.read(4)
-version = io.read(1).unpack("c")[0]
+io.read(4) # MPS7
+io.read(1).unpack("c")[0] # version number
 num_of_records = io.read(4).unpack("N")[0]
 
 # parse records
 num_of_records.times do |i|
  type_enum = io.read(1)
  timestamp = io.read(4).unpack("N")[0]
- uid = io.read(8).unpack("Q")[0]
+ uid = io.read(8).unpack("Q>")[0]
 
  # log values
  puts "type enum: " + type_enum.unpack("c")[0].to_s
@@ -34,21 +34,17 @@ num_of_records.times do |i|
    puts "amount: " + amount.to_s
 
    if uid == 2456938384156277127
-     puts "Equals id 2456938384156277127"
-     user_balance += amount
-     puts "YYYYYYYYYYYYYYEEEEEEEEEEEEEEEESSSSSSSSSSSSSSSSSSSS!!!!!!!!!!!!!!!"
+     print "*********** UID MATCHED *************"
+     user_balance -= amount if type_enum == "\x00"
+     user_balance += amount if type_enum == "\x01"
    end
  end
 
- if type_enum == "\x00" # debit
-   total_debit_amount += amount
- elsif type_enum == "\x01" # credit
-   total_credit_amount += amount
- elsif type_enum == "\x02" # auto started
-   autopays_started += 1
- elsif type_enum == "\x03" # auto ended
-   autopays_ended += 1
- end
+ # increment values
+ total_debit_amount += amount if type_enum == "\x00" # debit
+ total_credit_amount += amount if type_enum == "\x01" # credit
+ autopays_started += 1 if type_enum == "\x02" # auto started
+ autopays_ended += 1 if type_enum == "\x03" # auto ended
 
  puts "**********************************"
 end
